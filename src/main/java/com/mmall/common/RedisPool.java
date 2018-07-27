c3f36169a62d5018d1b1df7a671275ffd1c1ac73
backlocl <897135278@qq.com>
2018-07-27 09:53:58 +0800

    修改了最大等待时间和最少等待时间

--- src/main/java/com/mmall/common/RedisPool.java
+++ src/main/java/com/mmall/common/RedisPool.java
@@ -9,8 +9,8 @@ public class RedisPool {
 
     private static JedisPool pool=null;
     private static Integer maxTool= Integer.parseInt(PropertiesUtil.getProperty("redis.max.total","20"));
-    private static Integer maxIdle=Integer.parseInt(PropertiesUtil.getProperty("redis.max.idle","20"));;
-    private static Integer minIdle=Integer.parseInt(PropertiesUtil.getProperty("redis.min.idle","20"));;
+    private static Integer maxIdle=Integer.parseInt(PropertiesUtil.getProperty("redis.max.idle","10"));;
+    private static Integer minIdle=Integer.parseInt(PropertiesUtil.getProperty("redis.min.idle","2"));;
     private static Boolean testOnBorrow=Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.borrow","true"));
     private static Boolean testOnreturn=Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.return","true"));
 
